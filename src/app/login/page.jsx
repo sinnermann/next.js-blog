3c82bd95a22a2d66bef3e93871c6f2af2a3afc0a
sinnermann <ealijev@gmail.com>
2023-11-30 17:42:19 +0400

    Updates

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,18 @@
 "use client";
+import { signIn, useSession } from "next-auth/react";
 //import { signIn, useSession } from "next-auth/react";
 import styles from "./loginPage.module.css";
 import { useRouter } from "next/navigation";
 
-const page = () => {
+const LoginPage = () => {
+  const {data, status} = useSession()
+
+
+  console.log(data, status)
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton} >
+        <div className={styles.socialButton} onClick={()=>signIn("google")}>
           Sign in with Google
         </div>
         <div className={styles.socialButton}>Sign in with Github</div>
@@ -17,4 +22,4 @@ const page = () => {
   )
 }
 
-export default page
+export default LoginPage
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>Our Logo</div>
+      <div className={styles.logo}>Travel Blog</div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link href="/" className={styles.link}>Homepage</Link>
